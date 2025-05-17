b5e1ca2a4d1c2c38b718be4a7ff5b69acd93d26e
P-joyce <joyceachen10@gmail.com>
2025-05-17 10:50:19 +0300

    added sir

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.Write("What is your first name?; ");
         string firstName = Console.ReadLine();
 
-        Console.Write("What is your last name? ");
+        Console.Write("What is your last name?, sir");
         string lastName = Console.ReadLine();
 
         Console.WriteLine($"Hello, {lastName}, {firstName} {lastName}");
